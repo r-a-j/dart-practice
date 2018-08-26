50b5ace0c2c882318239cd8d07425dfc5e5f3ee8
Raj Pawar <er.rajpawar@gmail.com>
2018-08-27 01:51:20 +0530

    variables used

--- bin/main.dart
+++ bin/main.dart
@@ -1,5 +1,6 @@
 import 'package:dart_practice/dart_practice.dart' as dart_practice;
 
 main(List<String> arguments) {
-  print('Hello world: ${dart_practice.calculate()}!');
+  var name = "Raj Pawar";
+  print(name);
 }
